217ba9760fc7e17a92b0501eb6a3d3401a44a323
kanechan25 <kt.itengineer@gmail.com>
2025-06-02 14:14:07 +0700

    init project

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { CounterStoreProvider } from '@/provider/counterProvider'
+import { QueryProvider } from '@/provider/queryProvider'
+import { routers } from '@/routes/routes'
+import './assets/css/App.css'
+import mixpanel from 'mixpanel-browser'
+
+if (import.meta.env.NODE_ENV === 'production') {
+  mixpanel.init(import.meta.env.VITE_MIXPANEL_TOKEN, {
+    track_pageview: true,
+    persistence: 'localStorage',
+    debug: false,
+  })
+}
+
+function App() {
+  return (
+    <QueryProvider>
+      <CounterStoreProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route>
+              {routers.map((route) => (
+                <Route key={route.id} path={route.href} element={route.element} />
+              ))}
+              <Route path='*' element={<Navigate to='/' replace />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </CounterStoreProvider>
+    </QueryProvider>
+  )
+}
+
+export default App
---
+++ src/components/Common/Button.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+interface ButtonProps {
+  onClick: () => void
+  children: React.ReactNode
+}
+
+const Button = ({ onClick, children }: ButtonProps) => {
+  return (
+    <button className='bg-blue-300 text-white p-2 rounded hover:bg-blue-600 cursor-pointer' onClick={onClick}>
+      {children}
+    </button>
+  )
+}
+
+export default Button
